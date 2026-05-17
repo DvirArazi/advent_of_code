e61bca949b2f460b411e24c56ac68fc932a97c40
DvirArazi <108592948+DvirArazi@users.noreply.github.com>
2026-05-17 20:42:06 +0300

    completed 2025 5-1

--- solutions/2025/day05/part1.dart
+++ solutions/2025/day05/part1.dart
@@ -1,3 +1,25 @@
 Object solve(String input) {
-  return 0;
+  final sections = input.trim().split('\n\n');
+
+  final ranges = sections[0].split('\n').map((line) {
+    final parts = line.split('-');
+    return (start: int.parse(parts[0]), end: int.parse(parts[1]));
+  }).toList();
+
+  final ids = sections[1].split('\n').map((line) => int.parse(line)).toList();
+
+  var count = 0;
+  // print(ids);
+  // print(ranges);
+
+  for (var id in ids) {
+    for (var range in ranges) {
+      if (id >= range.start && id <= range.end) {
+        count++;
+        break;
+      }
+    }
+  }
+
+  return count;
 }
--- solutions/2025/day05/part2.dart
+++ solutions/2025/day05/part2.dart
@@ -1,3 +1,31 @@
 Object solve(String input) {
-  return 0;
+  final sections = input.trim().split('\n\n');
+
+  final ranges = sections[0].split('\n').map((line) {
+    final parts = line.split('-');
+    return (start: int.parse(parts[0]), end: int.parse(parts[1]));
+  }).toList();
+
+  var max = 0;
+
+  for (var i = 0; i < ranges.length; i++) {
+    if (max < ranges[i].end) {
+      max = ranges[i].end;
+    }
+  }
+
+  print(max);
+
+  var count = 0;
+
+  for (var i = 0; i <= max; i++) {
+    for (var range in ranges) {
+      if (i >= range.start && i <= range.end) {
+        count++;
+        break;
+      }
+    }
+  }
+
+  return count;
 }
